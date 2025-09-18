89ad2ca05ae59051502ae739c9ee870369e5e2e2
Rasika Thakur <125433090+rasikathakur@users.noreply.github.com>
2025-09-18 20:26:03 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -11,11 +12,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_MEASURE_ID,
 };
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
 export const googleProvider = new GoogleAuthProvider();
 export default app;
